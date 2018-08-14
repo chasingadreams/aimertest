0c34872fea5500e5a92653ebdfa95a18f2c3f1c6
chasi <chasi@DESKTOP-1LLU0MV>
2018-08-14 17:26:26 +0800

    gjk

--- src/IntegratTest.java
+++ src/IntegratTest.java
@@ -1,10 +1,12 @@
 
 
+import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class IntegratTest {
+	@Test
 	 public static void main(String[] args) {
 		 
 	        Result result = JUnitCore.runClasses(CalculateTest.class, UseInfoTest.class);
