d02455f4abad84f165358628f284f07a6738e5bf
chasi <chasi@DESKTOP-1LLU0MV>
2018-08-14 19:58:08 +0800

    aimer

--- src/CalculateTest.java
+++ src/CalculateTest.java
@@ -47,7 +47,7 @@ public class CalculateTest {
     {
         System.out.println("this is testSubstract()...");  
         int result = Calculate.substract(12, 2);
-        assertEquals("减法有问题", 15, result); 
+        assertEquals("减法有问题", 10, result); 
     }
 
     @Test
