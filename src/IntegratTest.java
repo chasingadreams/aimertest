73ea2bec1e3a2056c64323df841e6a8b65060dcc
chasi <chasi@DESKTOP-1LLU0MV>
2018-08-14 17:29:53 +0800

    aimer

--- src/IntegratTest.java
+++ src/IntegratTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class IntegratTest {
-	@Test
 	 public static void main(String[] args) {
 		 
 	        Result result = JUnitCore.runClasses(CalculateTest.class, UseInfoTest.class);
