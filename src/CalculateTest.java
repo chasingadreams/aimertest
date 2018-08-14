bc677dcf28a3d764389478bbc1513893c3ea76d7
chasi <chasi@DESKTOP-1LLU0MV>
2018-08-14 20:34:41 +0800

    aimer

--- src/CalculateTest.java
+++ src/CalculateTest.java
@@ -2,37 +2,9 @@
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculateTest {
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception 
-    {
-         System.out.println("this is setUpBeforeClass()...");  
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception 
-    {
-         System.out.println("this is tearDownAfterClass()...");  
-    }
-
-    @Before
-    public void setUp() throws Exception 
-    {
-        System.out.println("this is setUp()...");
-    }
-
-    @After
-    public void tearDown() throws Exception
-    {
-        System.out.println("this is tearDown()...");
-    }
 
     @Test
     public void testAdd() 
